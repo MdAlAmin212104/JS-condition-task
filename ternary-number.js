c1b2bd5c985aa0ddc9c0dcfe8660693331fd8347
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-01-28 12:25:25 +0600

    condition and ternary operator with solved

--- ternary-number.js
+++ ternary-number.js
@@ -10,3 +10,16 @@ write a simple if-else.
 also, write it using ternary operator.
 
  */
+
+const num1 = 15, num2 = 100;
+if( num1 > num2 ){
+      const result = num1 * 2;
+      console.log(result);
+}else{
+      const result = num1 + num2;
+      console.log(result);
+}
+
+// Ternary operator
+const result = (num1 > num2) ? ( num1*2):(num1+num2);
+console.log(result);
